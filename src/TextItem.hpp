70a8e8884e4f87edbdaad289ee531f237796bfc9
Alessandro Partesotti <alessandro.partesotti@fracarro.com>
2024-01-24 15:58:28 +0100

    text item virtual desc

--- src/TextItem.hpp
+++ src/TextItem.hpp
@@ -7,7 +7,7 @@ public:
     TextItem(std::string text, std::shared_ptr<MenuItem> parent);
     TextItem(std::string text, std::shared_ptr<MenuItem> parent, std::shared_ptr<MenuItem> left, std::shared_ptr<MenuItem> right);
     std::string GetText() const override;
-    ~TextItem() = default;
+    virtual ~TextItem() = default;
 
 private:
     std::string m_text;
